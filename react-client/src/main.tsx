a1bf18e205797d3d7f3bfdeb7307bfa2cac3ed19
symphony-annaLlorens <annallorens_12@hotmail.com>
2023-05-10 12:58:59 +0200

    Connect client to graphql server

--- react-client/src/main.tsx
+++ react-client/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+
+
+
+import { Client, Provider, cacheExchange, fetchExchange } from "urql";
+
+const client = new Client({
+  url: "http://localhost:4000/graphql",
+  exchanges: [cacheExchange, fetchExchange],
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider value={client}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
