88f65ec95237b5b26fd2b05f5a87a1357ebe1748
symphony-annaLlorens <annallorens_12@hotmail.com>
2023-05-10 12:33:37 +0200

    Added yoga server

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,3 +1,14 @@
-// src/index.ts
+import { createServer } from "node:http";
+import { createYoga } from "graphql-yoga";
+import { schema } from "./schema";
 
-console.log("Hey there! 👋");
+// Create a Yoga instance with a GraphQL schema.
+const yoga = createYoga({ schema });
+const port = Number(process.env.API_PORT) || 4000;
+
+// Pass it into a server to hook into request handlers.
+const server = createServer(yoga);
+
+server.listen(port, () => {
+  console.info(`Server is running on http://localhost:${port}/graphql`);
+});
